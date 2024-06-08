b5115603fec30b6d560b67ba613fbf7b315e13c3
Juan Carlos <juan.constantino1202@gmail.com>
2024-06-08 17:42:35 -0600

    ci(login): configuracion para hacer peticiones

--- src/views/Login/loginView.vue
+++ src/views/Login/loginView.vue
@@ -1,24 +1,41 @@
 <script setup>
 
-    import { reactive, ref } from 'vue';
+import { reactive, ref } from 'vue';
 
-    import Logo from '/src/components/Logo.vue';
-    import Alerta from '/src/components/Alerta.vue';
+import Logo from '/src/components/Logo.vue';
+import Alerta from '/src/components/Alerta.vue';
+import { authenticateUser } from '/src/api/AuthAPI.js'
+import { useMutation } from '@tanstack/vue-query'
+import { useRouter } from 'vue-router'
 
-    const error = ref('');
+const errors = ref('');
 
-    const dataLogin = reactive({
-        usuario: '',
-        password: ''
-    });
+const dataLogin = reactive({
+    correo: '',
+    password: ''
+});
 
-    const login = () => {
-        if(Object.values(dataLogin).includes('')){
-            error.value = 'Todos los campos son obligatorios';
-            return
-        }
-        error.value = '';
+const router = useRouter()
+
+const { mutate } = useMutation({
+    mutationFn: authenticateUser,
+    onError: (error) => {
+        errors.value = error
+    },
+    onSuccess: (data) => {
+        router.push('/')
     }
+})
+
+const login = async () => {
+    if (Object.values(dataLogin).includes('')) {
+        errors.value = 'Todos los campos son obligatorios';
+        return
+    }
+
+    mutate(dataLogin)
+
+}
 
 </script>
 
@@ -38,51 +55,34 @@
             </div>
 
             <!-- Form -->
-            <form 
-                @submit.prevent = "login"
-                class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col"
-            >
+            <form @submit.prevent="login" class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col">
 
                 <h1 class="uppercase text-white text-3xl font-extrabold text-center my-4">Ingresa tus Datos</h1>
 
-                <Alerta 
-                    v-if="error"
-                >
-                    {{ error }}
+                <Alerta v-if="errors">
+                    {{ errors }}
                 </Alerta>
 
                 <!-- Campo Usuario-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Usuario</h3>
-                    <input 
-                        type="text" 
-                        placeholder="INGRESA TÚ USUARIO"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold"
-                        v-model="dataLogin.usuario"
-                    >
+                    <input type="text" placeholder="INGRESA TÚ USUARIO"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold" v-model="dataLogin.correo">
                 </div>
                 <!-- Campo Contraseña-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Contraseña</h3>
-                    <input 
-                        type="text" 
-                        placeholder="INGRESA TÚ CONTRASEÑA"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold"
-                        v-model="dataLogin.password"
-                    >
+                    <input type="password" placeholder="INGRESA TÚ CONTRASEÑA"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold" v-model="dataLogin.password">
                 </div>
                 <!-- Botones -->
                 <div class="flex justify-center items-center flex-col my-3 gap-6 mb-20">
-                    <input 
-                        type="submit" 
-                        value="iniciar sesión"
-                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600"
-                    >
+                    <input type="submit" value="iniciar sesión"
+                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600">
 
                     <RouterLink
                         class="uppercase text-white font-normal hover:font-bold text-lg lg:m-3 border-b-2 border-red-600 w-full xl:w-1/2 text-center"
-                        :to="{ name: '' }"
-                    >
+                        :to="{ name: '' }">
                         Recuperar contraseña
                     </RouterLink>
                 </div>
