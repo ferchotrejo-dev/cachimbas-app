d19c336e783c145d8565394fca8b652df47acfd1
luistrejo-dev <L20280734@toluca.tecnm.mx>
2024-06-08 15:31:19 -0600

    build(alerta): alerta para el login

--- src/views/Login/loginView.vue
+++ src/views/Login/loginView.vue
@@ -1,6 +1,24 @@
 <script setup>
 
-import Logo from '/src/components/Logo.vue';
+    import { reactive, ref } from 'vue';
+
+    import Logo from '/src/components/Logo.vue';
+    import Alerta from '/src/components/Alerta.vue';
+
+    const error = ref('');
+
+    const dataLogin = reactive({
+        usuario: '',
+        password: ''
+    });
+
+    const login = () => {
+        if(Object.values(dataLogin).includes('')){
+            error.value = 'Todos los campos son obligatorios';
+            return
+        }
+        error.value = '';
+    }
 
 </script>
 
@@ -20,31 +38,55 @@ import Logo from '/src/components/Logo.vue';
             </div>
 
             <!-- Form -->
-            <div class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col">
+            <form 
+                @submit.prevent = "login"
+                class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col"
+            >
+
                 <h1 class="uppercase text-white text-3xl font-extrabold text-center my-4">Ingresa tus Datos</h1>
+
+                <Alerta 
+                    v-if="error"
+                >
+                    {{ error }}
+                </Alerta>
+
                 <!-- Campo Usuario-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Usuario</h3>
-                    <input type="text" placeholder="INGRESA TÚ USUARIO"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold">
+                    <input 
+                        type="text" 
+                        placeholder="INGRESA TÚ USUARIO"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold"
+                        v-model="dataLogin.usuario"
+                    >
                 </div>
                 <!-- Campo Contraseña-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Contraseña</h3>
-                    <input type="text" placeholder="INGRESA TÚ CONTRASEÑA"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold">
+                    <input 
+                        type="text" 
+                        placeholder="INGRESA TÚ CONTRASEÑA"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold"
+                        v-model="dataLogin.password"
+                    >
                 </div>
                 <!-- Botones -->
                 <div class="flex justify-center items-center flex-col my-3 gap-6 mb-20">
-                    <input type="button" value="iniciar sesión"
-                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600">
+                    <input 
+                        type="submit" 
+                        value="iniciar sesión"
+                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600"
+                    >
+
                     <RouterLink
                         class="uppercase text-white font-normal hover:font-bold text-lg lg:m-3 border-b-2 border-red-600 w-full xl:w-1/2 text-center"
-                        :to="{ name: '' }">
+                        :to="{ name: '' }"
+                    >
                         Recuperar contraseña
                     </RouterLink>
                 </div>
-            </div>
+            </form>
         </div>
 
     </section>
