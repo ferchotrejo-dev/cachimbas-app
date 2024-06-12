5545b8bcd3f2342f3a5ee9f7a62cddf8a3c34e40
luistrejo-dev <L20280734@toluca.tecnm.mx>
2024-06-12 10:03:25 -0600

    feat(login): cambio y corrección de colores en inputs

--- src/views/Login/loginView.vue
+++ src/views/Login/loginView.vue
@@ -1,41 +1,42 @@
 <script setup>
 
-import { reactive, ref } from 'vue';
+    import { reactive, ref } from 'vue';
+    import { authenticateUser } from '/src/api/AuthAPI.js'
+    import { useMutation } from '@tanstack/vue-query'
+    import { useRouter } from 'vue-router'
 
-import Logo from '/src/components/Logo.vue';
-import Alerta from '/src/components/Alerta.vue';
-import { authenticateUser } from '/src/api/AuthAPI.js'
-import { useMutation } from '@tanstack/vue-query'
-import { useRouter } from 'vue-router'
+    import Logo from '/src/components/Logo.vue';
+    import Alerta from '/src/components/Alerta.vue';
+    
 
-const errors = ref('');
+    const errors = ref('');
 
-const dataLogin = reactive({
-    correo: '',
-    password: ''
-});
+    const dataLogin = reactive({
+        correo: '',
+        password: ''
+    });
 
-const router = useRouter()
+    const router = useRouter()
 
-const { mutate } = useMutation({
-    mutationFn: authenticateUser,
-    onError: (error) => {
-        errors.value = error
-    },
-    onSuccess: (data) => {
-        router.push('/')
+    const { mutate } = useMutation({
+        mutationFn: authenticateUser,
+        onError: (error) => {
+            errors.value = error
+        },
+        onSuccess: (data) => {
+            router.push('/')
+        }
+    })
+
+    const login = async () => {
+        if (Object.values(dataLogin).includes('')) {
+            errors.value = 'Todos los campos son obligatorios';
+            return
+        }
+
+        mutate(dataLogin)
+
     }
-})
-
-const login = async () => {
-    if (Object.values(dataLogin).includes('')) {
-        errors.value = 'Todos los campos son obligatorios';
-        return
-    }
-
-    mutate(dataLogin)
-
-}
 
 </script>
 
@@ -66,19 +67,30 @@ const login = async () => {
                 <!-- Campo Usuario-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Usuario</h3>
-                    <input type="text" placeholder="INGRESA TÚ USUARIO"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold" v-model="dataLogin.correo">
+                    <input 
+                        type="text" 
+                        placeholder="INGRESA TÚ USUARIO"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white" 
+                        v-model="dataLogin.correo"
+                    >
                 </div>
                 <!-- Campo Contraseña-->
                 <div class="flex flex-wrap my-6">
                     <h3 class="uppercase text-white font-bold text-xl">Contraseña</h3>
-                    <input type="password" placeholder="INGRESA TÚ CONTRASEÑA"
-                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold" v-model="dataLogin.password">
+                    <input 
+                        type="password" 
+                        placeholder="INGRESA TÚ CONTRASEÑA"
+                        class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white" 
+                        v-model="dataLogin.password"
+                    >
                 </div>
                 <!-- Botones -->
                 <div class="flex justify-center items-center flex-col my-3 gap-6 mb-20">
-                    <input type="submit" value="iniciar sesión"
-                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600">
+                    <input 
+                        type="submit" 
+                        value="iniciar sesión"
+                        class="w-full xl:w-1/2 border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600"
+                    >
 
                     <RouterLink
                         class="uppercase text-white font-normal hover:font-bold text-lg lg:m-3 border-b-2 border-red-600 w-full xl:w-1/2 text-center"
