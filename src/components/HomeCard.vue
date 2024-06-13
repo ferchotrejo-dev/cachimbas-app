591a684ea4cc4574f4b373cd8239be40dd0ec66c
luistrejo-dev <L20280734@toluca.tecnm.mx>
2024-06-13 10:45:15 -0600

    feat(index): solución de responsive y ajuste de contenido

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,12 +1,9 @@
-
-<script setup>
- 
-</script>
+<script setup></script>
 
 <template>
-
-    <div class=" border-4 border-red-600 rounded-lg p-6 my-6 mx-10 lg:mx-4 xl:mx-10 text-white font-bold text-justify">
-        <slot></slot>
-    </div>
-
+  <div
+    class="border-4 border-red-600 rounded-lg p-6 my-6 mx-10 lg:mx-4 2xl:mx-10 text-white font-medium text-center text-sm 2xltext-md"
+  >
+    <slot></slot>
+  </div>
 </template>
