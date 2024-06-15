6824b763e33540c38fca1b5dfbffe7a7746b804a
luistrejo-dev <L20280734@toluca.tecnm.mx>
2024-06-14 22:06:19 -0600

    feat(admin-dashboard): se arreglo en tamaño del contenedor

--- src/views/admin/CreateUserView.vue
+++ src/views/admin/CreateUserView.vue
@@ -49,60 +49,96 @@ const createUser = async () => {
 </script>
 
 <template>
-    <section class="w-full p-2 flex justify-center">
-        <div class="bg-gray-950 w-full md:w-3/4 rounded-3xl h-full flex flex-wrap">
-            <form @submit.prevent="createUser" class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col">
-                <h1 class="uppercase text-white text-3xl font-extrabold text-center my-4">Nuevo Usuario</h1>
+    
+    <div class="grid col-span-11 ">
 
-                <Alerta v-if="errors">
-                    {{ errors }}
-                </Alerta>
+        <section class="w-full flex justify-center">
+            <div class="bg-gray-950 w-full rounded-3xl h-full flex flex-wrap">
+                <form @submit.prevent="createUser" class="w-full lg:w-1/2 p-16 flex justify-center flex-wrap flex-col">
+                    <h1 class="uppercase text-white text-3xl font-extrabold text-center my-4">Nuevo Usuario</h1>
 
-                <Success v-if="success">
-                    {{ success }}
-                </Success>
+                    <Alerta v-if="errors">
+                        {{ errors }}
+                    </Alerta>
 
-                <div>
-                    <div class="flex flex-wrap my-6">
-                        <label for="nombre" class="uppercase text-white font-bold text-xl">Nombre del Usuario</label>
-                        <input id="nombre" type="text" placeholder="NOMBRE"
-                            class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
-                            v-model="newUserData.nombre">
+                    <Success v-if="success">
+                        {{ success }}
+                    </Success>
+
+                    <div>
+                        <div class="flex flex-wrap my-6">
+                            <label for="nombre" class="uppercase text-white font-bold text-xl">Nombre del Usuario</label>
+                            <input 
+                                id="nombre" 
+                                type="text" 
+                                placeholder="NOMBRE USUARIO"
+                                class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
+                                v-model="newUserData.nombre"
+                            >
+                        </div>
+
+                        <div class="flex flex-wrap my-6">
+                            <label 
+                                for="apellido" 
+                                class="uppercase text-white font-bold text-xl"
+                            >Apellido del Usuario
+                            </label>
+                            <input 
+                                id="apellido" 
+                                type="text" 
+                                placeholder="APELLIDO USUARIO"
+                                class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
+                                v-model="newUserData.apellido">
+                        </div>
+
+                        <div class="flex flex-wrap my-6">
+                            <label 
+                                for="correo" 
+                                class="uppercase text-white font-bold text-xl"
+                            >
+                                Correo del Usuario
+                            </label>
+                            <input 
+                                id="correo" 
+                                type="text" 
+                                placeholder="CORREO USUARIO"
+                                class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
+                                v-model="newUserData.correo">
+                        </div>
+
+                        <div class="flex flex-wrap my-6">
+                            <label 
+                                for="password_confirmation" 
+                                class="uppercase text-white font-bold text-xl"
+                            >
+                                Rol
+                            </label>
+                            <select 
+                                id="rol" 
+                                class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
+                                v-model="newUserData.rol">
+                                <option value="">-- Selecciona una Opcion --</option>
+                                <option 
+                                    v-for="rol in data" :key="rol._id" :value="rol._id.toString()"
+                                >
+                                    {{ rol.nombre_rol }}
+                                </option>
+
+                            </select>
+                        </div>
                     </div>
+                    <input 
+                        type="submit" 
+                        value="Crear Usuario"
+                        class="w-full border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600"
+                    >
+                </form>
+                <div class="bg-img-4 bg-cover bg-center  rounded-3xl rounded-l-none w-full lg:w-1/2 border-blue-500">
 
-                    <div class="flex flex-wrap my-6">
-                        <label for="apellido" class="uppercase text-white font-bold text-xl">Apellido del
-                            Usuario</label>
-                        <input id="apellido" type="text" placeholder="APELLIDO"
-                            class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
-                            v-model="newUserData.apellido">
-                    </div>
-
-                    <div class="flex flex-wrap my-6">
-                        <label for="correo" class="uppercase text-white font-bold text-xl">Correo del Usuario</label>
-                        <input id="correo" type="text" placeholder="CORREO"
-                            class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
-                            v-model="newUserData.correo">
-                    </div>
-
-                    <div class="flex flex-wrap my-6">
-                        <label for="password_confirmation" class="uppercase text-white font-bold text-xl">Rol</label>
-                        <select id="rol" class="bg-gray-800 rounded-xl text-center p-2 w-full font-bold text-white"
-                            v-model="newUserData.rol">
-                            <option value="">-- Selecciona una Opcion --</option>
-                            <option v-for="rol in data" :key="rol._id" :value="rol._id.toString()">{{ rol.nombre_rol }}
-                            </option>
-
-                        </select>
-                    </div>
                 </div>
-                <input type="submit" value="Crear Usuario"
-                    class="w-full border-2 border-blue-600 text-white uppercase rounded-xl p-3  text-xl font-bold hover:bg-blue-600">
-            </form>
-            <div class="bg-img-4 bg-cover bg-center  rounded-3xl rounded-l-none w-full lg:w-1/2 border-blue-500">
-
             </div>
-        </div>
-    </section>
+        </section>
+
+    </div>
 
 </template>
